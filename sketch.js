5db86b480ab046b157f6f7c16fee7ecc7a6d6441
Vineet Kumar Doshi <vineetvivid@gmail.com>
2016-01-25 22:20:27 +0530

    Republic Day Special

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ var from = URL_SPLITLIST[2];
 var to_FONT;
 var message_FONT;
 var from_FONT;
-
+var bg_color;
 function preload(){
   to_FONT = loadFont("Quicksand-Bold.otf");
   message_FONT = loadFont("Quicksand-Regular.otf");
@@ -26,7 +26,8 @@ function preload(){
 }
 function setup() {
   canvas = createCanvas(can_width, can_height);
-  background(255);
+  bg_color = color( 0, 0, 128);
+  background( bg_color);
 }
 
 function mousePressed() {
@@ -41,8 +42,17 @@ function mousePressed() {
 
 function touchMoved(){
     noStroke();
-    fill(map(touchX, 0, can_width, 150, 200),0,map(touchY, 0, can_height, 150, 255));
-    ellipse(touchX, touchY, can_height/3, can_height/3);
+    if (touchY < can_height/3) {
+      fill(255,map(touchY, 0, can_height/3, 153,255),map(touchY, 0, can_height/3, 51,255));
+    }
+    else if (touchY > can_height/3*2){
+      fill(map(touchY, can_height/3*2, can_height, 255,0),map(touchY, can_height/3*2, can_height, 255, 100),map(touchY, can_height/3*2, can_height, 255, 8));
+    }
+    ellipse(touchX+random(-10,10), touchY+random(-10,10), can_height/10, can_height/10);
+    ellipse(touchX+random(-20,10), touchY+random(-20,10), can_height/10, can_height/10);
+    ellipse(touchX+random(-10,20), touchY+random(-10,20), can_height/10, can_height/10);
+    ellipse(touchX+random(-30,10), touchY+random(-30,10), can_height/10, can_height/10);
+    ellipse(touchX+random(-10,30), touchY+random(-10,30), can_height/10, can_height/10);
 }
 
 function draw(){
@@ -50,16 +60,24 @@ function draw(){
   if(toggle%2 == 0){
     
     noStroke();
-    
-    fill(map(mouseX, 0, can_width, 150, 200),0,map(mouseY, 0, can_height, 150, 255));
-    ellipse(mouseX, mouseY, can_height/3, can_height/3);
+    if (mouseY < can_height/3) {
+      fill(255,map(mouseY, 0, can_height/3, 153,255),map(mouseY, 0, can_height/3, 51,255));
+    }
+    else if (mouseY > can_height/3*2){
+      fill(map(mouseY, can_height/3*2, can_height, 255,0),map(mouseY, can_height/3*2, can_height, 255, 100),map(mouseY, can_height/3*2, can_height, 255, 8));
+    }
+    ellipse(mouseX+random(-10,10), mouseY+random(-10,10), can_height/10, can_height/10);
+    ellipse(mouseX+random(-20,10), mouseY+random(-20,10), can_height/10, can_height/10);
+    ellipse(mouseX+random(-10,20), mouseY+random(-10,20), can_height/10, can_height/10);
+    ellipse(mouseX+random(-30,10), mouseY+random(-30,10), can_height/10, can_height/10);
+    ellipse(mouseX+random(-10,30), mouseY+random(-10,30), can_height/10, can_height/10);
     cursor(CROSS);
   } else {
     cursor(HAND);
     
   }
 
-  fill(255)
+  fill(bg_color);
   //To
   textSize(can_height/10);
   textFont(to_FONT);
@@ -81,5 +99,5 @@ function windowResized() {
   can_width = windowWidth/1.1;
   can_height = windowHeight/1.5;
   resizeCanvas(can_width, can_height);
-  background(255);
+  background(bg_color);
 }
